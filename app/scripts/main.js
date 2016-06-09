16423c153f02dfb5fc49927709149e771078ea41
Mayya Sedova <mayya.sedova@gmail.com>
2016-06-09 16:05:47 -0700

    Added toolbar and data to display trees for other proteins.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,235 +1,280 @@
-var viewer = new Cesium.Viewer('cesiumContainer');
-var pinBuilder = new Cesium.PinBuilder();
+(function () {
+    'use strict';
 
-var countries = {};
-var strains = {};
-var tree = {};
-var minElevation = 100;
-var scaleFactor = 5000000;
+    var bingApiKey = 'AmynqN9p9KKkkF7tf-o1Ie1QWB2YSc0x9DqjbS8VuUabJ5Sps4lubTHJ6ieVpb0V'; // non-profit key, might need to change it later
+
+    Cesium.BingMapsApi.defaultKey = bingApiKey;
+
+    var defaultAction;
+
+    /* */
+    var PhyloGlobe = {
+        addToolbarMenu: function (options, toolbarID) {
+            var menu = document.createElement('select');
+            menu.className = 'cesium-button';
+            menu.onchange = function () {
+                PhyloGlobe.reset();
+                var item = options[menu.selectedIndex];
+                if (item && typeof item.onselect === 'function') {
+                    item.onselect();
+                }
+            };
+            document.getElementById(toolbarID || 'toolbar').appendChild(menu);
+
+            if (!defaultAction && typeof options[0].onselect === 'function') {
+                defaultAction = options[0].onselect;
+            }
+
+            for (var i = 0, len = options.length; i < len; ++i) {
+                var option = document.createElement('option');
+                option.textContent = options[i].text;
+                option.value = options[i].value;
+                menu.appendChild(option);
+            }
+        },
+        reset: function () {
+        }
+    };
 
 
-var entities = viewer.entities;
-var ptree = entities.add(new Cesium.Entity());
-var pins = entities.add(new Cesium.Entity());
+    var options = [
+        {
+            text: 'C',
+            onselect: function () {
+                createModel('C');
+            }
+        }, {
+            text: 'prM',
+            onselect: function () {
+                createModel('prM');
+            }
+        }, {
+            text: 'E',
+            onselect: function () {
+                createModel('E');
+            }
+        }, {
+            text: 'NS5',
+            onselect: function () {
+                createModel('NS5');
+            }
+        }, {
+            text: 'NS4B',
+            onselect: function () {
+                createModel('NS4B');
+            }
+        }
+    ];
 
-$.ajax({
-    type: 'GET',
-    url: 'data/countries.csv',
-    dataType: 'text',
-    success: function (data) {
-        //https://developers.google.com/public-data/docs/canonical/countries_csv
-        var carray = $.csv.toObjects(data);
-        carray.forEach(function (c) {
-            countries[c.name] = {lat: c.latitude, lon: c.longitude};
+    PhyloGlobe.addToolbarMenu(options);
+
+    /** general vars*/
+    var viewer = new Cesium.Viewer('cesiumContainer', {
+        imageryProvider: new Cesium.ArcGisMapServerImageryProvider({
+            url: 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer'
+        })
+    }
+    );
+    var pinBuilder = new Cesium.PinBuilder();
+    var entities = viewer.entities;
+    var minElevation = 100;
+    var scaleFactor = 5000000;
+
+    var countries = {};
+
+    var strains = {};
+
+    var ptree = entities.add(new Cesium.Entity());
+
+    $.ajax({
+        type: 'GET',
+        url: 'data/countries.csv',
+        dataType: 'text',
+        success: function (data) {
+            //https://developers.google.com/public-data/docs/canonical/countries_csv
+            var carray = $.csv.toObjects(data);
+            carray.forEach(function (c) {
+                countries[c.name] = {lat: c.latitude, lon: c.longitude};
+            });
+            console.log('Loaded geo data');
+            createModel('NS5');
+        }
+    });
+
+
+
+    function createModel(name) {
+         console.log('Loading data for protein '+name);
+        entities.removeAll();
+        loadMetaData(name);
+    }
+
+    function createModelFromInput(metadata, treedata) {
+        entities.removeAll();
+        processDatapoints(metadata);
+        console.log('Loaded meta data');
+
+        var tree = parseNewick(data);
+        console.log(tree);
+        console.log('Loaded tree data');
+    }
+
+
+    function loadMetaData(name) {
+        $.ajax({
+            type: 'GET',
+            url: 'data/zika' + name + '.txt',
+            dataType: 'text',
+            success: function (data) {
+                processDatapoints(data);
+                console.log('Loaded meta data');
+                loadTree(name);
+            }
         });
-        console.log('Loaded geo data');
-        loadMetaData();
     }
-});
 
-function loadMetaData() {
-    $.ajax({
-        type: 'GET',
-        url: 'data/zikaNS5.txt',
-        dataType: 'text',
-        success: function (data) {
-            processDatapoints(data);
-            console.log('Loaded meta data');
-            loadTree();
-        }
-    });
-}
-
-function loadTree() {
-    $.ajax({
-        type: 'GET',
-        url: 'data/zikaNS5.nwk',
-        dataType: 'text',
-        success: function (data) {
-            var tree = parseNewick(data);
-            console.log(tree);
-            console.log('Loaded tree data');
-        }
-    });
-}
+    function loadTree(name) {
+        $.ajax({
+            type: 'GET',
+            url: 'data/zika' + name + '.nwk',
+            dataType: 'text',
+            success: function (data) {
+                var tree = parseNewick(data);
+                console.log(tree);
+                console.log('Loaded tree data');
+            }
+        });
+    }
 
 
-function processDatapoints(csv) {
-    var header = '"Accession","Length","Country","Host","Date","Unkn","CollectionDate","Definition","gi","GenomeRegion"';
-    console.log('required metadata header:\n' + header);
-    var datapoints = $.csv.toObjects(header + '\n' + csv);//, {delimiter:" ", separator:"\t"});
-    datapoints.forEach(function (d) {
-        var c = countries[d.Country];
-        if (c === null) {
-            console.log('Unable to determine location lat:lon for ' + d.Country + '. Will use 0:0');
-            d.lat = 0;
-            d.lon = 0;
-        } else {
-            d.lat = c.lat;
-            d.lon = c.lon;
-        }
-        strains[d.Accession] = d;
-        addPin(d);
-    });
+    function processDatapoints(csv) {
+        var header = '"Accession","Length","Country","Host","Date","Unkn","CollectionDate","Definition","gi","GenomeRegion"';
+        console.log('required metadata header:\n' + header);
+        var datapoints = $.csv.toObjects(header + '\n' + csv);//, {delimiter:" ", separator:"\t"});
+        datapoints.forEach(function (d) {
+            var c = countries[d.Country];
+            if (c) {
+                d.lat = c.lat;
+                d.lon = c.lon;
+            } else {
+                console.log('Unable to determine location lat:lon for ' + d.Country + '. Will use 0:0');
+                d.lat = 0;
+                d.lon = 0;
+
+            }
+            strains[d.Accession] = d;
+        });
 //    console.log(datapoints);
-}
-
-function addPin(d) {
-
-//    var entity = entities.add({
-//        parent: pins,
-//        position: Cesium.Cartesian3.fromDegrees(d.lon, d.lat),
-//        label: {
-//            text: d.Country,
-//            verticalOrigin: Cesium.VerticalOrigin.TOP
-//        },
-//        billboard: {
-//            image: pinBuilder.fromColor(Cesium.Color.RED, 24).toDataURL(),
-//            verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
-//        }
-//    });
-}
-
-
-function parseNewick(newick) {
-    var t = new Tree();
-    newick = newick.trim(newick).replace(/ /g, '');
-    t.Parse(newick);
-    if (t.error !== 0)
-    {
-        console.log('Error parsing tree');
-    } else
-    {
-        t.ComputeDepths();
-        assignCoordinates(t.root);
-        draw(t);
     }
 
-    return t;
-}
+    function parseNewick(newick) {
+        var t = new Tree();
+        newick = newick.trim(newick).replace(/ /g, '');
+        t.Parse(newick);
+        if (t.error !== 0)
+        {
+            console.log('Error parsing tree');
+        } else
+        {
+            t.ComputeDepths();
+            assignCoordinates(t.root);
+            draw(t);
+        }
 
-function assignCoordinates(node) {
-    if (node.xyz) {
+        return t;
+    }
+
+    function assignCoordinates(node) {
+        if (node.xyz) {
+            return node.xyz;
+        }
+
+        if (node.IsLeaf()) {
+            var strain = strains[node.label];
+            node.xyz = strain === null ? [0, 0, minElevation] : [parseFloat(strain.lat), parseFloat(strain.lon), minElevation + node.edge_length * scaleFactor];
+
+        } else {
+            var left = assignCoordinates(node.child);
+            var right = assignCoordinates(node.child.sibling);
+            node.xyz = [(left[0] + right[0]) / 2,
+                (left[1] + right[1]) / 2,
+                Math.max(parseFloat(left[2]) + parseFloat(right[2])) + node.edge_length * scaleFactor];
+//        console.log("left: " + left + ", right: " + right + " node: " + node.xyz);
+        }
         return node.xyz;
     }
 
-    if (node.IsLeaf()) {
-        var strain = strains[node.label];
-        node.xyz = strain === null ? [0, 0, minElevation] : [parseFloat(strain.lat), parseFloat(strain.lon), minElevation + node.edge_length * scaleFactor];
 
-    } else {
-        var left = assignCoordinates(node.child);
-        var right = assignCoordinates(node.child.sibling);
-        node.xyz = [(left[0] + right[0]) / 2,
-            (left[1] + right[1]) / 2,
-            Math.max(parseFloat(left[2]) + parseFloat(right[2])) + node.edge_length * scaleFactor];
-//        console.log("left: " + left + ", right: " + right + " node: " + node.xyz);
+
+    function draw(tree) {
+
+        drawEdges(tree.root, tree.root.depth);
+        tree.nodes.forEach(function (n) {
+            if (n.IsLeaf()) {
+                var entity = entities.add({
+                    parent: ptree,
+                    position: Cesium.Cartesian3.fromDegrees(n.xyz[1], n.xyz[0], n.xyz[2]),
+                    label: {
+                        text: n.label,
+                        font: '14pt monospace',
+                        verticalOrigin: Cesium.VerticalOrigin.TOP
+                    },
+                    billboard: {
+                        image: pinBuilder.fromColor(Cesium.Color.GREEN, 28).toDataURL(),
+                        verticalOrigin: Cesium.VerticalOrigin.BOTTOM
+                    }
+                });
+            } else {
+                var entity = entities.add({
+                    parent: ptree,
+                    position: Cesium.Cartesian3.fromDegrees(n.xyz[1], n.xyz[0], n.xyz[2]),
+                    point: {
+                        color: Cesium.Color.BLUE,
+                        pixelSize: 6
+                    }
+                });
+            }
+        });
+
+
+        ptree.show = true;
     }
-    return node.xyz;
-}
 
 
+    function drawEdges(node, maxdepth) {
+        if (node === null)
+            return;
 
-function draw(tree) {
+        if (node.child) {
+            drawEdges(node.child, maxdepth);
+            drawEdges(node.child.sibling, maxdepth);
+        }
 
-    drawEdges(tree.root, tree.root.depth);
-    tree.nodes.forEach(function (n) {
-        if (n.IsLeaf()) {
-            var entity = entities.add({
+        var edgeColor = Cesium.Color.BLUE.brighten(1 - 100 * node.depth / maxdepth, new Cesium.Color());
+
+        var parent = node.ancestor;
+        if (parent) {
+            var line = viewer.entities.add({
                 parent: ptree,
-                position: Cesium.Cartesian3.fromDegrees(n.xyz[1], n.xyz[0], n.xyz[2]),
-                label: {
-                    text: n.label,
-                    font: '14pt monospace',
-                    verticalOrigin: Cesium.VerticalOrigin.TOP
-                },
-                billboard: {
-                    image: pinBuilder.fromColor(Cesium.Color.GREEN, 28).toDataURL(),
-                    verticalOrigin: Cesium.VerticalOrigin.BOTTOM
+                polyline: {
+                    positions: Cesium.Cartesian3.fromDegreesArrayHeights([node.xyz[1], node.xyz[0], node.xyz[2],
+                        parent.xyz[1], parent.xyz[0], parent.xyz[2]]),
+                    width: 3,
+                    material: edgeColor
                 }
             });
         } else {
-            var entity = entities.add({
+            var line = viewer.entities.add({
                 parent: ptree,
-                position: Cesium.Cartesian3.fromDegrees(n.xyz[1], n.xyz[0], n.xyz[2]),
-                point: {
-                    color: Cesium.Color.BLUE,
-                    pixelSize: 6
+                polyline: {
+                    positions: Cesium.Cartesian3.fromDegreesArrayHeights([node.xyz[1], node.xyz[0], node.xyz[2],
+                        node.xyz[1], node.xyz[0], node.xyz[2] + scaleFactor / 10]),
+                    width: 3,
+                    material: edgeColor
                 }
             });
         }
-    });
-
-
-    ptree.show = true;
-
-
-//viewer.dataSources.add(Cesium.GeoJsonDataSource.load('data/world-topo.json'));
-
-}
-
-
-function drawEdges(node, maxdepth) {
-    if (node === null)
-        return;
-
-    if (node.child) {
-        drawEdges(node.child, maxdepth);
-        drawEdges(node.child.sibling, maxdepth);
     }
 
-    var edgeColor = Cesium.Color.BLUE.brighten(1 - 100 * node.depth / maxdepth, new Cesium.Color());
 
-    var parent = node.ancestor;
-    if (parent) {
-        var line = viewer.entities.add({
-            parent: ptree,
-            polyline: {
-                positions: Cesium.Cartesian3.fromDegreesArrayHeights([node.xyz[1], node.xyz[0], node.xyz[2],
-                    parent.xyz[1], parent.xyz[0], parent.xyz[2]]),
-                width: 3,
-                material: edgeColor
-            }
-        });
-    } else {
-        var line = viewer.entities.add({
-            parent: ptree,
-            polyline: {
-                positions: Cesium.Cartesian3.fromDegreesArrayHeights([node.xyz[1], node.xyz[0], node.xyz[2],
-                    node.xyz[1], node.xyz[0], node.xyz[2] + scaleFactor/10]),
-                width: 3,
-                material: edgeColor
-            }
-        });
-    }
-}
-
-
-
-//
-//var PhyloG = {};
-//
-//PhyloG.addToolbarMenu([{
-//        text: 'All flu cases',
-//        onselect: function () {
-//            viewer.camera.flyHome(0);
-//            viewer.dataSources.add(Cesium.KmlDataSource.load('../../SampleData/ge/all.kml', options));
-//        }
-//    },
-//    {text: 'human h5n1_he',
-//        onselect: function () {
-//            viewer.camera.flyHome(0);
-//            viewer.dataSources.add(Cesium.KmlDataSource.load('../../SampleData/ge/human_h5n1_HE.kml', options));
-//        }
-//    }, {text: 'human h5n1_NA',
-//        onselect: function () {
-//            viewer.camera.flyHome(0);
-//            viewer.dataSources.add(Cesium.KmlDataSource.load('../../SampleData/ge/human_h5n1_NA.kml', options));
-//        }
-//    }, ], 'toolbar');
-//
-//PhyloG.reset = function () {
-//    viewer.dataSources.removeAll();
-//    viewer.clock.clockRange = Cesium.ClockRange.UNBOUNDED;
-//    viewer.clock.clockStep = Cesium.ClockStep.SYSTEM_CLOCK;
-//};
+})();
